bfe8880d4977b8ac2eaed51fcddcd4935cac8c8f
Kai <kailinasc2@gmail.com>
2025-10-30 16:43:26 -0300

    index update

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,11 @@
+import todoStore from '../store/todo.store';
 import html from './app.html?raw';
+import { renderTodos } from './use-cases';
+
+const ElementIDs = {
+    Todolist: '.todo-list',
+}
+
 
 /**
  * 
@@ -6,12 +13,19 @@ import html from './app.html?raw';
  */
 export const App = ( elementID ) =>{
 
-// cuando la aplicacion aoo se llama   
-( ()=>{  
-    const app = document.createElement('div');
-    app.innerHTML = html;
-    document.querySelector(elementID).append(app);  
+    const displayTodos = () =>
+    {
+       const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+       renderTodos( ElementIDs.Todolist, todos );
+        
+    }
 
-})();
+    // cuando la aplicacion aoo se llama   
+    ( ()=>{  
+        const app = document.createElement('div');
+        app.innerHTML = html;
+        document.querySelector(elementID).append(app);  
+
+    })();
 
 }
---
+++ src/todos/use-cases/render-todos.js
@@ -0,0 +1,9 @@
+import { Todo } from "../models/todo.model";
+
+export const renderTodos = ( elementID ,todos = [] ) =>
+{
+    const element = document.querySelector( elementID );
+    todos.forEach( todo => {
+
+    });
+}
