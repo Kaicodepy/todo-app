2a9f56dc487aeac18c95a26d37fae931b78ead7d
Kai <kailinasc2@gmail.com>
2025-10-30 14:37:06 -0300

    todo implementatiton

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -11,6 +11,8 @@ const state = {
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del Poder'),
+        new Todo('Piedra del campo'),
     ],
     filter: Filters.All, 
 }
@@ -25,25 +27,75 @@ const loadStore = () =>
     throw new Error('Not implemented');
 }
 
+const getTodos = ( filter=Filters.All ) =>
+{
+    switch(filter)
+    {
+        case Filters.All:
+            return [...state.todos];
+        
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done);
+        
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done);
+        
+        default:
+            throw new Error(`Option ${filter} is not valid`);
+    }       
+}
+
 const addTodo = ( description ) =>
 {
-    throw new Error('Not implemented');
+    if (!description) throw new Error('Description is requiered');
+    state.todos.push(new Todo(description) );
 }
 
 const toggleTodo = (todoId) =>
-{
-    throw new Error('Not implemented');
+{   state.todos = state.todos.map(todo =>{ 
+
+    if( todo.id === todoId )
+    {
+        todo.done = !todo.done;
+    }
+    return todo;
+});
+    
 }
 
 const deleteTodo = ( todoId ) =>
 {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.id !== todoId );
 }
 
 const deleteCompleted = () =>
 {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.done );
 }
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) =>
+{
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () =>
+{
+    return state.filter;
+}
+
+
 export default {
+    addTodo,
     initStore,
+    deleteTodo,
+    deleteCompleted,
+    getCurrentFilter,
+    getTodos,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
