7817bc5f3f7b4028f31cbc819bde4c7d4ffc6799
Kai <kailinasc2@gmail.com>
2025-10-29 22:52:24 -0300

    uuid creation

--- src/todos/models/todo.model.js
+++ src/todos/models/todo.model.js
@@ -1,7 +1,13 @@
+
+
 export class Todo {
 
     constructor( description)
     {
-        this.id=1;
+        this.id = 1;
+        this.description = description;
+        this.done = false;
+        this.createdAt = new Date();
     }
+
 }
